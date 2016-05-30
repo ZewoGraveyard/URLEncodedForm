3987ca49f11c62dc154e65160c94130940612c3a
Tomohisa Takaoka <tomohisa@jtechs.com>
2016-05-30 14:00:02 +0900

    Updated for 05-09 snapshot
    Only dependencies changed

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "URLEncodedForm",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/StructuredData.git", majorVersion: 0, minor: 5),
-        .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 5)
+        .Package(url: "https://github.com/Zewo/StructuredData.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 7)
     ]
 )
